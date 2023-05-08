a77550a815f22b4b2a91da2c3a014fa09fb96447
yann <jeanmaireyann.pro@gmail.com>
2023-05-08 18:20:15 +0200

    ajout de apiProperty sur les dto user et auth

--- src/api/user/user.dto.ts
+++ src/api/user/user.dto.ts
@@ -1,11 +1,14 @@
 import { IsOptional, IsString } from 'class-validator';
+import {ApiProperty} from "@nestjs/swagger";
 
 export class UpdateNameDto {
+    @ApiProperty()
   @IsString()
   @IsOptional()
   public readonly name?: string;
 }
 export class UpdatePasswordDto {
+    @ApiProperty()
   @IsString()
   @IsOptional()
   public readonly password?: string;
