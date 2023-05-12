e36f3bbbddedcc206018ce2ab8565e18c2227496
yann <jeanmaireyann.pro@gmail.com>
2023-05-12 13:43:46 +0200

    ajout de la recupération et de la gestion des mangas favoris

---
+++ src/api/favorites/favorite.service.ts
@@ -0,0 +1,68 @@
+import {
+    Injectable,
+} from '@nestjs/common';
+
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import User from 'src/api/user/user.entity';
+import { Manga } from 'src/api/mangas/manga.entity';
+import {UserMangaFavorite} from "@/api/favorites/user-manga-favorite.entity";
+import {MangaQuickViewDto} from "@/api/mangas/dto/manga-quick-view.dto";
+
+@Injectable()
+export class FavoriteService {
+    @InjectRepository(Manga)
+    private readonly mangaRepository: Repository<Manga>;
+    @InjectRepository(UserMangaFavorite)
+    private readonly userFavoriteMangaRepository: Repository<UserMangaFavorite>;
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>;
+
+
+
+
+    async addFavoriteManga(mangaId: number, user: User): Promise<void> {
+        const manga: Manga = await this.mangaRepository.findOne({
+            where: { id: mangaId },
+            relations: ['favoriteMangas'],
+        });
+        const userFavoriteManga = new UserMangaFavorite();
+        userFavoriteManga.user = user;
+        userFavoriteManga.manga = manga;
+        if (await this.userFavoriteMangaRepository.findOneBy(userFavoriteManga)) {
+            return;
+        }
+        await this.userFavoriteMangaRepository.save(userFavoriteManga);
+    }
+    async getFavoriteManga(userId: number): Promise<MangaQuickViewDto[]> {
+        const userMangas = await this.mangaRepository
+            .createQueryBuilder('manga')
+            .leftJoinAndSelect(
+                UserMangaFavorite,
+                'usereMangaFavorite',
+                'usereMangaFavorite.manga_id = manga.id',
+            )
+            .leftJoinAndSelect(User, 'user', 'user.id = usereMangaFavorite.user_id')
+            .where('user.id = :id', { id: userId })
+            .getRawMany();
+
+        const nbMangas = userMangas.length;
+        const userMangasQuickView: MangaQuickViewDto[] = new Array(nbMangas);
+        for (let i = 0; i < nbMangas; i++) {
+            userMangasQuickView[i] = MangaQuickViewDto.fromLibrary(userMangas[i]);
+        }
+
+        return userMangasQuickView;
+    }
+
+    async deleteFavoriteManga(mangaId: number,user: User): Promise<void> {
+        const manga: Manga = await this.mangaRepository.findOne({
+            where: { id: mangaId },
+            relations: ['favoriteMangas'],
+        });
+        const userFavoriteManga = new UserMangaFavorite();
+        userFavoriteManga.user = user;
+        userFavoriteManga.manga = manga;
+        await this.userFavoriteMangaRepository.delete(userFavoriteManga)
+    }
+}
---
+++ src/api/favorites/user-manga-favorite.entity.ts
@@ -0,0 +1,23 @@
+import User from '@/api/user/user.entity';
+import {
+    Entity,
+    JoinColumn,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Manga } from '../mangas/manga.entity';
+
+@Entity()
+export class UserMangaFavorite {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ManyToOne(() => User, (user) => user.userMangas)
+    @JoinColumn({ name: 'user_id' })
+    user: User;
+
+    @ManyToOne(() => Manga, (manga) => manga.favoriteMangas)
+    @JoinColumn({ name: 'manga_id' })
+    manga: Manga;
+
+}
