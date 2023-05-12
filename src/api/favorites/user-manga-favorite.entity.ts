9bc6f2457097c024745fa7ed7aa79e9d6fe59523
suprasanity <suprasanity@users.noreply.github.com>
2023-05-12 12:10:48 +0000

    Auto Commit Code Formatting

--- src/api/favorites/user-manga-favorite.entity.ts
+++ src/api/favorites/user-manga-favorite.entity.ts
@@ -1,23 +1,17 @@
 import User from '@/api/user/user.entity';
-import {
-    Entity,
-    JoinColumn,
-    ManyToOne,
-    PrimaryGeneratedColumn,
-} from 'typeorm';
+import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Manga } from '../mangas/manga.entity';
 
 @Entity()
 export class UserMangaFavorite {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @ManyToOne(() => User, (user) => user.userMangas)
-    @JoinColumn({ name: 'user_id' })
-    user: User;
+  @ManyToOne(() => User, (user) => user.userMangas)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
 
-    @ManyToOne(() => Manga, (manga) => manga.favoriteMangas)
-    @JoinColumn({ name: 'manga_id' })
-    manga: Manga;
-
+  @ManyToOne(() => Manga, (manga) => manga.favoriteMangas)
+  @JoinColumn({ name: 'manga_id' })
+  manga: Manga;
 }
