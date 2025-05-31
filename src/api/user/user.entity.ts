029ba2e1c4ea89e67b038699158df6de840cb626
lbibal <louisbibal4835@gmail.com>
2025-05-31 13:02:08 +0200

    Removing the Favorites Service

--- src/api/user/user.entity.ts
+++ src/api/user/user.entity.ts
@@ -1,6 +1,5 @@
 import { UserManga } from 'src/api/mangas/user-manga.entity';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { UserMangaFavorite } from '@/api/favorites/user-manga-favorite.entity';
 
 @Entity()
 class User {
@@ -16,16 +15,11 @@ class User {
   @Column()
   password: string;
 
-  @OneToMany(
-    () => UserMangaFavorite,
-    (userFavoriteManga) => userFavoriteManga.user,
-  )
-  favoriteMangas: UserMangaFavorite[];
-
   @OneToMany(() => UserManga, (userManga) => userManga.user)
   user_mangas: UserManga[];
 
   @Column({ type: 'timestamp', nullable: true, default: null })
   public lastLoginAt: Date | null;
 }
+
 export default User;
